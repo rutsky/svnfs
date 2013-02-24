c158a260c774e46c63923f5ef3738b931c6c8172
Vladimir Rutsky <rutsky.vladimir@gmail.com>
2013-02-25 00:04:37 +0400

    Try to change revision time (without success)

--- tests/create_test_repo.sh
+++ tests/create_test_repo.sh
@@ -14,6 +14,23 @@ rm -rf $WC
 
 svnadmin create $REPO
 
+# Enable pre-revprop-change to allow changing commit time
+cp $REPO/hooks/pre-revprop-change.tmpl $REPO/hooks/pre-revprop-change
+cat <<EOF > $REPO/hooks/pre-revprop-change
+#!/bin/bash
+
+REPOS="$1"
+REV="$2"
+USER="$3"
+PROPNAME="$4"
+ACTION="$5"
+
+if [ "$ACTION" = "M" -a "$PROPNAME" = "svn:log" ]; then exit 0; fi
+
+exit 0 # Allow any property change
+EOF
+chmod +x $REPO/hooks/pre-revprop-change
+
 svn co svn+ssh://$REPO/ $WC
 
 pushd $WC > /dev/null
@@ -21,8 +38,10 @@ pushd $WC > /dev/null
 echo "Test file" > test.txt
 svn add test.txt
 svn ci -m "Add test.txt"
+svn propset svn:date --revprop -r 1 2013-00-00T00:00:00.000000Z
 
 echo "First change" > test.txt
 svn ci test.txt -m "First change"
+svn propset svn:date --revprop -r 2 2013-00-01T00:00:00.000000Z
 
 popd > /dev/null
